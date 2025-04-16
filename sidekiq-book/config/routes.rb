27eebe5ca3b6b259ac2e0501a91578a917c738fe
sriramvivekk <sriramvivekk@gmail.com>
2025-04-16 09:11:16 +0530

    changes

--- sidekiq-book/config/routes.rb
+++ sidekiq-book/config/routes.rb
@@ -1,5 +1,9 @@
+require "sidekiq/web" 
+
 Rails.application.routes.draw do
   resources :orders, only: [ :new, :create, :show ]
   resources :simulated_behaviors, only: [ :edit, :update ]
   root "welcome#show"
+
+  mount Sidekiq::Web => "/sidekiq"
 end
